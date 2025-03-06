d3f13a01bab5d89ef9e6325f20f32a08243bf484
Malywan Vereecque <malywan.vereecque@gmail.com>
2025-03-06 10:19:55 +0100

    Ajoutez des fichiers projet.

---
+++ ContentPipeline/TXT/TXTAsset.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCreator.core.Types.ContentPipeline.TXT
+{
+    public class TXTAsset
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+
+        public TXTAsset()
+        {
+            Name = string.Empty;
+            Content = string.Empty;
+        }
+        public TXTAsset(string name, string content)
+        {
+            Name = name;
+            Content = content;
+        }
+    }
+}
---
+++ ContentPipeline/TXT/TXTReader.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCreator.core.Types.ContentPipeline.TXT
+{
+    public class TXTReader : ContentTypeReader<TXTAsset>
+    {
+        protected override TXTAsset Read(ContentReader input, TXTAsset existingInstance)
+        {
+            string Name = input.ReadString();
+            string Content = input.ReadString();
+
+            TXTAsset asset = new TXTAsset(Name, Content);
+
+            return asset;
+        }
+    }
+}
